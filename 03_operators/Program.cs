c3dbc9d62c4091c8df03a0da1a151d45dbde3ead
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-04-25 10:49:36 +0300

    Logic operator example.

--- 03_operators/Program.cs
+++ 03_operators/Program.cs
@@ -30,3 +30,35 @@ a = 10;
 // postfix form
 //int c = a++ * 2; // 20
 int c = ++a * 2;   // 22
+
+// ---------- logic: > < >= <= == != 
+
+a = 5;
+b = 2;
+
+// logic operator return: true / false (bool)
+
+Console.WriteLine($"A > B: {a > b}");
+Console.WriteLine($"A < B: {a < b}");
+Console.WriteLine($"A >= B: {a >= b}");
+Console.WriteLine($"A <= B: {a <= b}");
+Console.WriteLine($"A == B: {a == b}");
+Console.WriteLine($"A != B: {a != b}");
+
+if (a > b) 
+{
+    // do this if condition is TRUE
+    Console.WriteLine("A is bigger than B!");
+}
+
+// logic combination: && (and) || (or)
+
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+// check for range (10...50)
+
+Console.WriteLine($"In range (10...50): {number >= 10 && number <= 50}");
+Console.WriteLine($"Out of range (10...50): {number < 10 || number > 50}");
+
+// logic negative: !
+Console.WriteLine($"Negative of A > B: {!(a > b)}");
