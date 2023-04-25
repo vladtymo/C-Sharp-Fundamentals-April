0c18a579b05388f405d4781461a07d3fd53dcb74
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-04-25 11:14:53 +0300

    Enum and structures example.

---
+++ 04_enum/Program.cs
@@ -0,0 +1,34 @@
+﻿//const int Forward = 1;
+//const int Right = 2;
+//const int Left = 3;
+//const int Backward = 4;
+
+// ------------------- enumeration (enum)
+enum Direction
+{
+    // start = 0, next = previous + 1
+    Forward = 1, Left, Right, Backward
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // create enum variable
+        Direction startDirection = Direction.Left;
+
+        Console.WriteLine("\tChoose direction:\n" +
+        "1 - Move Forward\n" +
+        "2 - Turn Rigth\n" +
+        "3 - Turn Left\n" +
+        "4 - Move Backward");
+
+        Console.Write("Your direction (1-4): ");
+        Direction direction = Enum.Parse<Direction>(Console.ReadLine());
+
+        if (direction == Direction.Forward)
+            Console.WriteLine("Your are moving North");
+        if (direction == Direction.Right)
+            Console.WriteLine("Your are moving East");
+    }
+}
---
+++ 05_structure/Program.cs
@@ -0,0 +1,39 @@
+﻿struct User
+{
+    public int ID { get; set; }
+    public string Email { get; set; }
+    public string? Address { get; set; }
+    public double Bonus { get; set; }
+    public bool IsAdmin { get; set; }
+
+    public override string ToString()
+    {
+        return $"[{ID}] - {Email} Address: {Address ?? "Bomj"} Bonus: {Bonus}";
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //int id = 1002;
+        //string email = "vlad@ukr.net";
+        //string address = "Rivne, Ukraine";
+        //double bonus = 50.5;
+        //bool isAdmin = false;
+
+        User me = new User()
+        {
+            ID = 1004,
+            Email = "vlad@ukr.net",
+            Bonus = 33.5,
+            IsAdmin = true
+        };
+        // name.member
+        me.Address = "Rivne, Ukraine";
+        //...
+
+        Console.WriteLine($"User ID: {me.ID}");
+        Console.WriteLine($"User information: {me}");
+    }
+}
